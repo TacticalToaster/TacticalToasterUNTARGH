7b2eb90da27c8525aa544c72df82bd1bac885a04
Matthew Christensen <christensenfamily05@gmail.com>
2025-09-29 14:39:01 -0500

    Added Brain Manager

---
+++ Plugin/Behavior/UNTARBrainManager.cs
@@ -0,0 +1,18 @@
+﻿using DrakiaXYZ.BigBrain.Brains;
+using EFT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacticalToasterUNTARGH.Behavior
+{
+    public static class UNTARBrainManager
+    {
+        public static void AddUntarBrainLayers()
+        {
+            //BrainManager.AddCustomLayer(, new List<string>() { "PMC", "ExUsec" }, 4, UNTAREnums.UNTARTypes.ConvertAll<WildSpawnType>(type => (WildSpawnType)type.wildSpawnType));
+        }
+    }
+}
