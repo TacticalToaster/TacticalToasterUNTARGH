acdd80ad72e647e018df343affe1d7eddce8ec8e
GrooveypenguinX <grooveypenguinx@gmail.com>
2026-09-16 13:46:15 -0400

    SAIN decoupling + version bump

--- Prepatch/ClientInfo.cs
+++ Prepatch/ClientInfo.cs
@@ -7,5 +7,5 @@ public static class ClientInfo
     public const string UNTARGHPreLoadGUID = "com.untarghprepatch.tacticaltoaster";
     public const string UNTARGHPluginName = "TacticalToaster-UNTARGH";
     public const string UNTARGHPreLoadName = "TacticalToaster-UNTARGHPrePatch";
-    public const string UNTARGHVersion = "3.2.1";
+    public const string UNTARGHVersion = "3.2.2";
 }
--- TacticalToasterUNTARGH/Mod.cs
+++ TacticalToasterUNTARGH/Mod.cs
@@ -18,13 +18,12 @@ public record ModMetadata : IModMetadata
     public string Name { get; init; } = "UNTAR Go Home!";
     public string Author { get; init; } = "TacticalToaster";
     public List<string>? Contributors { get; init; } = new() { };
-    public SemanticVersioning.Version Version { get; init; } = new(3, 2, 1);
+    public SemanticVersioning.Version Version { get; init; } = new(3, 2, 2);
     public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.5");
     public List<string>? Incompatibilities { get; init; }
     public Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = new()
     {
         { "com.morebotsapi.tacticaltoaster", new SemanticVersioning.Range(">=2.1.1") },
-        { "me.sol.sain", new SemanticVersioning.Range("~4.5.1") },
         { "com.wtt.commonlib", new SemanticVersioning.Range(">=3.0.0") }
     };
     public string? Url { get; init; }
